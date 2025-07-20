805fe80bbc424f5b11eaac9a05a5bc33b2f16abc
MuhammedIkbalKARADELI <karadeli2001@hotmail.com>
2025-07-20 23:00:04 +0300

    last changes an update

--- app/init_db.py
+++ app/init_db.py
@@ -1,5 +1,22 @@
 import psycopg2
 from config import POSTGRES_CONN_INFO
+import time
+import psycopg2
+
+def wait_for_postgres(dsn, timeout=60):
+    start = time.time()
+    while True:
+        try:
+            conn = psycopg2.connect(dsn)
+            conn.close()
+            print("PostgreSQL is up!")
+            return
+        except psycopg2.OperationalError:
+            if time.time() - start > timeout:
+                raise
+            print("Waiting for PostgreSQL...")
+            time.sleep(2)
+
 
 def create_rag_queries_table():
     conn = psycopg2.connect(POSTGRES_CONN_INFO)
@@ -19,7 +36,7 @@ def create_rag_queries_table():
     conn.commit()
     cur.close()
     conn.close()
-    print("rag_queries database was checked")
+    print("rag_log_queries database was checked")
 
 
 def create_query_documents_table():
@@ -28,7 +45,7 @@ def create_query_documents_table():
     cur.execute("""
     CREATE TABLE IF NOT EXISTS query_documents(
         id SERIAL PRIMARY KEY,
-        query_id INTEGER REFERENCES rag_queries(id) ON DELETE CASCADE,
+        query_id INTEGER REFERENCES rag_log_queries(id) ON DELETE CASCADE,
         pmid VARCHAR(32),
         sentence_index INTEGER,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -41,6 +58,8 @@ def create_query_documents_table():
 
 
 if __name__ == "__main__":
+    # Bağlantı bilgini gir:
+    wait_for_postgres(POSTGRES_CONN_INFO)
     # Bu kod bloğu, sadece ilgili Python dosyası DOĞRUDAN çalıştırıldığında çalışır. Eğer dosya bir modül olarak başka bir dosyada import edilirse, bu blok çalışmaz.
     create_rag_queries_table()
     create_query_documents_table()
